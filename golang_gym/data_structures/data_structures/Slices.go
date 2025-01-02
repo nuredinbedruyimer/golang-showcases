f177e749b9d88e817c142125d5a9fc1255fc0bb0
nuredinbedruyimer <nuredinbedru06@gmail.com>
2025-01-02 12:00:00 +0300

    Slices Exercise

--- golang_gym/data_structures/data_structures/Slices.go
+++ golang_gym/data_structures/data_structures/Slices.go
@@ -15,3 +15,43 @@ func SliceCust(){
 		fmt.Println(index, name)
 	}
 }
+
+func ReverseSlice(arr[]int){
+	left := 0
+	right := len(arr) - 1
+
+	for left < right{
+		arr[left], arr[right] = arr[right], arr[left]
+		left += 1
+		right -= 1
+	}
+
+	
+}
+func RemoveAllEven(arr[]int)[]int{
+	var size int = (len(arr) + 1) / 2
+	ans := make([]int, 0, size)
+
+	for _ , curr_value := range arr{
+		if curr_value % 2 == 1{
+			ans = append(ans, curr_value)
+		}
+	}
+	return ans
+
+}
+func RotateLeft(arr[]int, k int) []int{
+	N := len(arr)
+	ans := make([]int, 0 , N)
+	k = k % N
+
+	for index := k; index < N; index++{
+		ans=append(ans, arr[index])
+
+	}
+	for index := 0; index < k; index ++{
+		ans = append(ans, arr[index])
+	}
+	return ans
+
+}
--- golang_gym/data_structures/main.go
+++ golang_gym/data_structures/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	datastructures "github.com/nuredinbedru/data_structures/data_structures"
 )
 
 func main(){
-	datastructures.SliceCust()	
+
+	arr := []int{1, 2, 3, 4, 5}
+	k := 7
+	fmt.Println(datastructures.RotateLeft(arr, k))
+	
+
 	
 }
