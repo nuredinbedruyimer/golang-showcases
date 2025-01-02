11cc2891bef0a9d57b2578aaf056a288890e0a0f
nuredinbedruyimer <nuredinbedru06@gmail.com>
2025-01-02 12:00:00 +0300

    Golang Showcases

---
+++ golang_gym/data_structures/Array.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+
+func main(){
+	var scores[3] float64
+	students := [4] string{"Luffy", "Sanji", "Brook", "Zoro"}
+
+	scores[0] = 89
+	scores[1] = 89
+	scores[2] = 89
+
+	fmt.Println("Scores : ", scores)
+	fmt.Println("Names : ", students)
+
+
+
+}
+
+
